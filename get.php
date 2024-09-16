65ac48e5751e955cfcd94d444d4d444fb69e55ca
malucario <122913162+malucario@users.noreply.github.com>
2024-09-16 12:26:27 -0500

    Update get.php

--- get.php
+++ get.php
@@ -4,7 +4,7 @@ Include "view-header.php";
 
 <h1>Get</h1>
 <form method = "get" action="get-result.php">
-  <input type="text" name="my-name">
+  <input type="text" name="team-name">
   <input type="submit" value="get data">
  <?php
 Include "view-footer.php";
