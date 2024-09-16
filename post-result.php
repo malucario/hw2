01e289410053c55844cd10dfe9f238bc30640152
malucario <122913162+malucario@users.noreply.github.com>
2024-09-16 11:13:06 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -1,16 +1,14 @@
 <?php
 Include "view-header.php";
 ?>
-
 <h1>Post result</h1>
-
- <?php
+<?php
 echo getDisplay();
 Include "view-footer.php";
 
 function getDisplay() {
  if (isset($_POST['my-name'])) {
-  return "<p>The value sent is:</p>" . $_POST["my-name"]
+  return "<p>The value sent is:</p>" . $_POST["my-name"];
 } else {
   return "<p>Nothing posted to the page.</p>";
 }
