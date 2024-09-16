5dffcdb4a4195a8585da1d6af0b71c4e8a19de16
malucario <122913162+malucario@users.noreply.github.com>
2024-09-16 11:17:27 -0500

    Update post.php

--- post.php
+++ post.php
@@ -3,8 +3,9 @@ Include "view-header.php";
 ?>
 
 <h1>Post</h1>
+  <p>In all lowercase, type in the name of the team you would like to see the matchup for!</p>
 <form method = "post" action="post-result.php">
-  <input type="text" name="my-name">
+  <input type="text" name="team-name">
   <input type="submit" value="post data">
  <?php
 Include "view-footer.php";
