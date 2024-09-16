7dec3b64ff63104af9eb895e56a52df02dd3908d
malucario <122913162+malucario@users.noreply.github.com>
2024-09-16 11:35:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ Include "view-header.php";
 ?>
 
 <h1>Week 3 SEC Scoreboard</h1>
- <p>If you're curious about the football scores across out new conference this past weekend, head over to the Post or Get pages to ask about some scores!</p>
+ <p>If you're curious about the football scores across our new conference this past weekend, head over to the Post or Get pages to ask about some scores!</p>
  <?php
 Include "view-footer.php";
 ?>
