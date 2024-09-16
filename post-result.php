c9a65e0029b126b701a56ec542aba6b97854fc4d
malucario <122913162+malucario@users.noreply.github.com>
2024-09-16 11:01:13 -0500

    Create post-result.php

---
+++ post-result.php
@@ -0,0 +1,19 @@
+<?php
+Include "view-header.php";
+?>
+
+<h1>Post result</h1>
+
+ <?php
+if (isset($_POST['my-name'])) {
+?>
+  <p>The value sent is:</p>
+<?php
+  echo $_POST["my-name"];
+} else {
+?>
+  <p>Nothing posted to the page></p>
+<?php
+}
+Include "view-footer.php";
+?>
