08df21f20e94474774372ed44e2395cb3963aae1
malucario <122913162+malucario@users.noreply.github.com>
2024-09-16 10:57:52 -0500

    Create post.php

---
+++ post.php
@@ -0,0 +1,11 @@
+<?php
+Include "view-header.php";
+?>
+
+<h1>Post</h1>
+<form method = "post" action="post-result.php">
+  <input type="text" name="my-name">
+  <input type="submit" value="post data">
+ <?php
+Include "view-footer.php";
+?>
