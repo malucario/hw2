34e2bb7cc075655549d25a333448c8b8640de1e7
malucario <122913162+malucario@users.noreply.github.com>
2024-09-16 11:03:55 -0500

    Create get.php

---
+++ get.php
@@ -0,0 +1,11 @@
+<?php
+Include "view-header.php";
+?>
+
+<h1>Get</h1>
+<form method = "get" action="get-result.php">
+  <input type="text" name="my-name">
+  <input type="submit" value="get data">
+ <?php
+Include "view-footer.php";
+?>
